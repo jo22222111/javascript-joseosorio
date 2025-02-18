5390b5b1b4638911cf3f4e9f8fb0fda1bc0d7048
jose osorio <josemiguelosorio1234@gmail.com>
2025-02-18 11:17:46 -0500

    se agrego a javascript

---
+++ script.js
@@ -0,0 +1,26 @@
+alert("probando el script de manera externa");
+
+//Ejercicio 1 
+
+let nombre = "jose osorio";
+let edad = 18;
+let ciudad= "garzón-huila";
+console.log ("mi nombre es" + nombre + ", tengo " + edad + "años y vivo en" + ciudad);
+
+/* Crea una variable llamada precio y asignale un valor decimal.
+luego crea otra variable llamada cantidad y asignale un valor entero.
+muestra el tipo de dato de cada variable en la consola. */
+
+
+let precio = 3.14;
+let cantidad = 5;
+console. log (typeof precio); // Imprime en la consola 
+document.write(typeof cantidad); // Imprime en el navegador
+alert(typeof cantidad ); // Imprime en una ventana emergente 
+
+
+
+/* Una tienda ofrece un 25% de descuento por cada compra. Un cliente desea
+saber cuánto deberá pagar por su compra. */
+
+
